3fe2b9592ada66a717588e52bda2223bf65b7a56
Sarp <burak.sarp@turkcell.com.tr>
2016-11-19 16:29:44 +0300

    initial commit

--- src/main/java/com/sarp/dynamodb/config/DynamodbConfig.java
+++ src/main/java/com/sarp/dynamodb/config/DynamodbConfig.java
@@ -16,19 +16,19 @@ import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 
 @Configuration
 public class DynamodbConfig {
-	
+
 	@Bean
-	public DynamoDBMapper dynamoDBMapper() throws IOException  {
-        return new DynamoDBMapper(this.amazonDynamoDB());
+	public DynamoDBMapper dynamoDBMapper() throws IOException {
+		return new DynamoDBMapper(this.amazonDynamoDB());
 	}
-	
+
 	private AmazonDynamoDB amazonDynamoDB() throws IOException {
 		final InputStream properties = Thread.currentThread().getContextClassLoader()
-                .getResourceAsStream("classpath:aws/aws.properties");
+				.getResourceAsStream("classpath:aws/aws.properties");
 		final AWSCredentials propertiesCredentials = new PropertiesCredentials(properties);
-        final AmazonDynamoDB amazonDynamoDb = new AmazonDynamoDBClient(propertiesCredentials);
-        amazonDynamoDb.setRegion(Region.getRegion(Regions.US_WEST_2));
-        return amazonDynamoDb;
+		final AmazonDynamoDB amazonDynamoDb = new AmazonDynamoDBClient(propertiesCredentials);
+		amazonDynamoDb.setRegion(Region.getRegion(Regions.US_WEST_2));
+		return amazonDynamoDb;
 	}
 
 }
